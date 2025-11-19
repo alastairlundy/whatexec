597df55a644d8eabba6bc2f942bd78a828c6b65e
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-11-19 18:30:28 +0000

    Update UserInputHelper.cs

--- src/WhatExec.Cli/Helpers/UserInputHelper.cs
+++ src/WhatExec.Cli/Helpers/UserInputHelper.cs
@@ -1,11 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using AlastairLundy.WhatExec.Cli.Localizations;
-using AlastairLundy.WhatExec.Cli.Localizations;
-using AlastairLundy.WhatExec.Cli.Localizations;
-using Spectre.Console;
 
 namespace AlastairLundy.WhatExec.Cli.Helpers;
 
